57de4730ae1d106ac3d4de62c2446d230a7b3ab0
TY <42424141+rhmtty@users.noreply.github.com>
2026-03-10 11:14:56 +0700

    Refactor keycloak-add-users-group.sh for parameterized input

--- keycloak-add-users-group.sh
+++ keycloak-add-users-group.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-CONTAINER={container-name}
-REALM={realm-name}
-GROUP_ID={group-id}
+CONTAINER=$1
+REALM=$2
+GROUP_ID=$3
+USER_FILE=$4
+
+if [ -z "$CONTAINER" ] || [ -z "$REALM" ] || [ -z "$GROUP_ID" ] || [ -z "$USER_FILE" ]; then
+  echo "Usage: $0 <container> <realm> <group_id> <user_file>"
+  exit 1
+fi
 
 while read USER
 do
- USER_ID=$(docker exec $CONTAINER /opt/keycloak/bin/kcadm.sh get users -r $REALM -q username=$USER --fields id | jq -r '.[0].id')
+  USER_ID=$(docker exec $CONTAINER \
+  /opt/keycloak/bin/kcadm.sh get users -r $REALM -q username=$USER --fields id \
+  | jq -r '.[0].id')
 
- if [ ! -z "$USER_ID" ]; then
-   docker exec $CONTAINER /opt/keycloak/bin/kcadm.sh update users/$USER_ID/groups/$GROUP_ID -r $REALM -n
+  if [ "$USER_ID" != "null" ] && [ ! -z "$USER_ID" ]; then
+    docker exec $CONTAINER \
+    /opt/keycloak/bin/kcadm.sh update users/$USER_ID/groups/$GROUP_ID -r $REALM -n
 
-   echo "Added $USER"
- else
-   echo "User $USER not found"
- fi
+    echo "Added $USER to group"
+  else
+    echo "User $USER not found"
+  fi
 
-done < users.csv
+done < "$USER_FILE"
